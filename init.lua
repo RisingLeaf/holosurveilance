7f9f5ababeca3c293380aab369b381ab54f70e4b
thvk-net <control@thvk.net>
2023-05-31 15:54:52 -0700

    highlight

--- init.lua
+++ init.lua
@@ -16,6 +16,7 @@ minetest.register_entity(mod_name..":holoblock", {
 		end
 	end
 })
+
 minetest.register_entity(mod_name..":holoplayer", {
 	initial_properties = {
 		physical = true,
@@ -23,6 +24,23 @@ minetest.register_entity(mod_name..":holoplayer", {
 		visual_size = {x = 0.0625, y = 0.0625, z = 0.0625},
 		collisionbox = {0, 0, 0, 0, 0, 0},
 		textures = {"holoplayer.png", "holoplayer.png", "holoplayer.png", "holoplayer.png", "holoplayer.png", "holoplayer.png"}
+	},
+	on_step = function(self, dtime)
+		if self.lifetime and self.lifetime <= 0 then
+			self.object:remove()
+		else
+			self.lifetime = (self.lifetime or 2) - dtime
+		end
+	end
+})
+
+minetest.register_entity(mod_name..":holoregion", {
+	initial_properties = {
+		physical = true,
+		visual = "cube",
+		visual_size = {x = 16, y = 16, z = 16},
+		collisionbox = {0, 0, 0, 0, 0, 0},
+		textures = {"regionmarker.png", "regionmarker.png", "regionmarker.png", "regionmarker.png", "regionmarker.png", "regionmarker.png"}
 	},
 	on_step = function(self, dtime)
 		if self.lifetime and self.lifetime <= 0 then
@@ -46,12 +64,13 @@ minetest.register_node(mod_name..":holo", {
 	end,
 	on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
 		local meta = minetest.get_meta(pos)
-		local formspec = "size[5,4]" ..
+		local formspec = "size[5,5]" ..
 			"label[0.5,0.5;Scan Pos:]" ..
 			"field[1,2;1,1;x;X:;".. meta:get_int("X") .."]" ..
 			"field[2,2;1,1;y;Y:;".. meta:get_int("Y")  .."]" ..
 			"field[3,2;1,1;z;Z:;".. meta:get_int("Z")  .."]" ..
-			"button_exit[1,3.5;3,1;submit;Submit]"
+			"button[1,3;3,1;show;Toggle Highlight]"..
+			"button_exit[1,4.5;3,1;submit;Submit]"
 		clicker:get_meta():set_string("holonode", minetest.serialize(pos))
 		minetest.show_formspec(clicker:get_player_name(), "surveilance_area", formspec)
 	end,
@@ -99,6 +118,9 @@ minetest.register_abm({
 				minetest.add_entity(spos, mod_name..":holoplayer")
 			end
 		end
+		if meta:get_int("show") > 0 then
+			minetest.add_entity({x = scanpos.x + 8, y = scanpos.y + 8, z = scanpos.z + 8}, mod_name..":holoregion")
+		end
 	end
 })
 
@@ -111,6 +133,10 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 			meta:set_int("X", tonumber(fields["x"]))
 			meta:set_int("Y", tonumber(fields["y"]))
 			meta:set_int("Z", tonumber(fields["z"]))
+		elseif fields["show"] then
+			local pos = minetest.deserialize(player:get_meta():get_string("holonode"))
+			local meta = minetest.get_meta(pos)
+			meta:set_int("show", (meta:get_int("show") > 0) and 0 or 1)
 		end
 	end
 end)
